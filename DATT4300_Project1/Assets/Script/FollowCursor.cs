97c9f4072762f173a958d204690a5a84b9e24499
RaphaelDizonn <90470101+RaphaelDizonn@users.noreply.github.com>
2025-09-24 04:40:59 -0400

    Flashlight Effect + Box Collider Fix
    Added flashlight effect and edited box collider

---
+++ DATT4300_Project1/Assets/Script/FollowCursor.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class FollowCursor : MonoBehaviour
+{
+    void Update()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        mousePos.z = 0f;
+
+        transform.position = mousePos;
+    }
+}
